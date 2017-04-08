c6c09298d5e27d38f4369e7afd3d905ddd60c1b7
Teacher <teacher@shs-mclab-05.shrewsbury.k12.ma.us>
2017-04-08 11:55:06 -0400

    Initial Commit

---
+++ GeoQuestAdventure/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GeoQuestAdventure
+//
+//  Created by Teacher on 4/8/17.
+//  Copyright © 2017 SrinuL. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
